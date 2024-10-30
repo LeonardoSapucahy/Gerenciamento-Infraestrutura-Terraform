32357da599b7d842c1867e968fc050b691982b4d
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:17:49 -0300

    Update outputs.tf

--- terraform/aws/eks/dev/outputs.tf
+++ terraform/aws/eks/dev/outputs.tf
@@ -1,1 +1,13 @@
 # Definição de saídas para cluster dev
+
+output "cluster_endpoint" {
+  value = module.eks_dev.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks_dev.cluster_security_group_id
+}
+
+output "kubectl_config" {
+  value = module.eks_dev.kubectl_config
+}
