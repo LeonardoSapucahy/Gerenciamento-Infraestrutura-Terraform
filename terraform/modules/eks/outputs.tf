8c1b52d9e88c0a0244ee33e5abb0441e6e1751a3
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:09:22 -0300

    Update outputs.tf

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,1 +1,13 @@
 # Saídas do módulo EKS
+
+output "cluster_endpoint" {
+  value = module.eks_cluster.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks_cluster.cluster_security_group_id
+}
+
+output "kubectl_config" {
+  value = module.eks_cluster.kubeconfig
+}
