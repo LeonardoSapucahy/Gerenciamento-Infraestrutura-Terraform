056d24a1e9e2fc7ddb516b825be80f9035f3b594
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:19:00 -0300

    Update variables.tf

--- terraform/aws/eks/prod/variables.tf
+++ terraform/aws/eks/prod/variables.tf
@@ -1,1 +1,25 @@
 # Definição de variáveis para cluster prod
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+  default     = "eks-prod"
+}
+
+variable "subnets" {
+  description = "Subnets for the EKS cluster"
+  type        = list(string)
+  default     = ["subnet-0123456789abcdef2"]
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
+  default     = "vpc-0123456789abcdef2"
+}
