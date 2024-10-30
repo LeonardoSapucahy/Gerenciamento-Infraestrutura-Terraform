0e0ddbe3a21f405905d4df7a583c552620321a54
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:09:56 -0300

    Update variables.tf

--- terraform/modules/pods/variables.tf
+++ terraform/modules/pods/variables.tf
@@ -1,1 +1,28 @@
 # Variáveis do módulo Pods
+
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+}
+
+variable "namespace" {
+  description = "Namespace for the deployment"
+  type        = string
+  default     = "default"
+}
+
+variable "replicas" {
+  description = "Number of replicas"
+  type        = number
+  default     = 3
+}
+
+variable "container_image" {
+  description = "Container image to deploy"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Port that the container listens on"
+  type        = number
+}
