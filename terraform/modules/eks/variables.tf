ba28a35ab53568154cd89929f6d12b1873c68861
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:09:08 -0300

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,1 +1,21 @@
 # Variáveis do módulo EKS
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets for the EKS cluster"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
+}
