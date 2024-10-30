4f284997a21ed724d404eece056313e71f4ff572
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 13:18:42 -0300

    Update outputs.tf

--- terraform/aws/eks/hml/outputs.tf
+++ terraform/aws/eks/hml/outputs.tf
@@ -1,1 +1,13 @@
 # Definição de saídas para cluster hml
+
+output "cluster_endpoint" {
+  value = module.eks_hml.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks_hml.cluster_security_group_id
+}
+
+output "kubectl_config" {
+  value = module.eks_hml.kubectl_config
+}
